ec755668584ed35995e24e71e0123a9ae090f1a4
vipings <vipingsnair@icloud.com>
2025-08-03 11:07:52 +0530

    blog: publish latest posts from vault

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-VAULT_DIR=~/obsidean-viping-dev/viping.dev
+VAULT_DIR="/Users/vpillai/Library/Mobile Documents/iCloud~md~obsidian/Documents/viping.dev"
 ZOLA_DIR=~/viping-dev/content/blog
 
 # Sync blog posts, exclude .obsidian/ and templates/
@@ -11,12 +12,12 @@ rsync -av --update \
 
 cd ~/viping-dev
 
-# Optional: verify Zola builds
-zola build
+# Zola build
+/usr/local/bin/zola build
 
-# Commit and push
+# Git commit + push
 git add content/blog/
-git commit -m "blog: publish latest posts from vault"
+git commit -m "blog: publish latest posts from vault" --no-gpg-sign
 git push origin main
 
 echo "✅ Blog updated and deployed!"
